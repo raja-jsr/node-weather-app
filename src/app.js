db0563a74839c21b4e6f665378b7064c62384ae8
Raja Chakraborty <r.chakraborty2009@gmail.com>
2020-07-12 14:21:13 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,121 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { error } = require('console')
+
+// Define path for express config
+const publicPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+
+// Setup handlebar engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to server
+app.use(express.static(publicPath)) // It will load a statick page
+
+// app.get('', (req, res)=>{
+//     res.send('<h1>Weather</h1>')
+// }) // This route never going to work because we are using app.use(express.static(publicPath)). 
+// It will load the index page page in the public directory
+
+// app.get('/help', (req, res)=>{
+//     res.send([{
+//         name: 'Raja'
+//     },
+//     {
+//         name: 'Andrew'
+//     }
+// ])
+// })
+
+// app.get('/about', (req, res)=>{
+//     res.send('<h1>About page</h1>')
+// })
+
+// if you want to use dynamic content with template engine
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Weather',
+        name: 'Raja Chakraborty'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Raja Chakraborty'
+    })
+})
+
+app.get('/help',(req, res)=>{
+    res.render('help',{
+        helpText: 'Hello There I am Raja to help you out',
+        title: 'Help',
+        name: 'Raja Chakraborty'
+    })
+})
+
+app.get('/weather', (req, res)=>{
+   if(!req.query.address) {
+        return res.send({
+            error: 'Address must be provided!'
+        })
+    }
+    
+    geocode(req.query.address, (error, {latitude, longitude, location } = {})=>{
+        if(error){
+            return res.send({error})
+        }
+        
+        forecast(latitude, longitude, (error, forecastData)=>{
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // res.send({
+    //     location: 'Philadalphia',
+    //     tempreture: '40 degree out there',
+    //     name: 'Raja Chakraborty',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products',(req, res)=>{
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        errorMsg: 'Help artical not found',
+        name: 'Raja Chakraborty'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        errorMsg: 'Page not found'
+    })
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is up on the port 3000');    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback)=>{
+    const url = 'http://api.weatherstack.com/current?access_key=617a96eee770914f93aa505291d7b4e1&query='+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)+'&units=f'
+       request( { url, json : true }, (error, {body}) => {
+            if(error){
+                callback('Unable to connect to weather service!', undefined);        
+            } else if(body.error) {
+                console.log(body);
+                
+                callback('Unable to find location!', undefined);
+            } else {
+                const curr = body.current
+                const temp = curr.temperature
+                const feelsLike = curr.feelslike
+                const weather_desc = curr.weather_descriptions[0]
+
+                callback(undefined, weather_desc + '. It is currently '+ temp + ' degrees out. It is feels like ' + feelsLike + ' degrees out.');
+            }
+    })
+}
+
+module.exports = forecast
+
